d9f0b4c3661f9fbb19a00c197af1bd69961d018a
ShambhuJha <shambhu.jha014@gmail.com>
2021-04-24 16:58:53 +0530

    Created project layout with css grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { TabsSectionComponent } from './shared/tabs-section/tabs-section.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { MainSectionComponent } from './shared/main-section/main-section.component';
+import { LayoutComponent } from './layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    TabsSectionComponent,
+    SidebarComponent,
+    MainSectionComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/tabs-section/tabs-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-section',
+  templateUrl: './tabs-section.component.html',
+  styleUrls: ['./tabs-section.component.scss']
+})
+export class TabsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
