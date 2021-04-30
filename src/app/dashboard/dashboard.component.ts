fad111dc378eb2a94b8790d39b25b2d041457845
ShambhuJha <shambhu.jha014@gmail.com>
2021-04-30 12:31:50 +0530

    Added new columnsn cell data= static and modified table style

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface StateOverview {
+  state: string;
+  confirmed: string;
+  active: string;
+  recovered: string;
+  deceased:string;
+  tested: string;
+  vaccinated: string;
+  population: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: StateOverview[] = [
+  {state: 'Delhi', confirmed: '12,34,345', active: '02,23,121', recovered: '10,23,232', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'west Bengal', confirmed: '45,67,543', active: '05,43,213', recovered: '11,23,212', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'Delhi', confirmed: '12,34,345', active: '02,23,121', recovered: '10,23,232', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'west Bengal', confirmed: '45,67,543', active: '05,43,213', recovered: '11,23,212', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'Delhi', confirmed: '12,34,345', active: '02,23,121', recovered: '10,23,232', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'west Bengal', confirmed: '45,67,543', active: '05,43,213', recovered: '11,23,212', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'Delhi', confirmed: '12,34,345', active: '02,23,121', recovered: '10,23,232', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'west Bengal', confirmed: '45,67,543', active: '05,43,213', recovered: '11,23,212', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'Delhi', confirmed: '12,34,345', active: '02,23,121', recovered: '10,23,232', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  {state: 'west Bengal', confirmed: '45,67,543', active: '05,43,213', recovered: '11,23,212', deceased:'2311', tested: '21,23,212', vaccinated: '11,23,190', population: '12,23,32,123'},
+  
 ];
 @Component({
   selector: 'app-dashboard',
@@ -24,7 +29,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['state', 'confirmed', 'active', 'recovered', 'deceased', 
+  'tested', 'vaccinated', 'population'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
