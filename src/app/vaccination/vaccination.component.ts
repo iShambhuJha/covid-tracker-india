4ec4a1c8423d183326cbbc705142c6dcaca90458
ShambhuJha <shambhu.jha014@gmail.com>
2021-05-07 14:38:07 +0530

    data binding-slot details

--- src/app/vaccination/vaccination.component.ts
+++ src/app/vaccination/vaccination.component.ts
@@ -12,6 +12,7 @@ export class VaccinationComponent implements OnInit {
   pinCode: any;
   datePicker: any;
   panelOpenState = false;
+  slotDetails:any=[];
   constructor(private _CovidAllData: CovidAllData) {}
 
   ngOnInit(): void {}
@@ -25,11 +26,13 @@ export class VaccinationComponent implements OnInit {
       }),
     };
     let params = new HttpParams();
-    params = params.append("pincode", this.pinCode);
+    params = params.append("pincode", '827013');
     params = params.append("date", "07-05-2021");
     const headers = new HttpHeaders().set("Accept", "*/*").set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
 
     const options = { params: params, headers: headers };
-    this._CovidAllData.getVaccineSlots(params).subscribe((res) => {});
+    this._CovidAllData.getVaccineSlots(params).subscribe((res) => {
+      this.slotDetails=res.centers;
+    });
   }
 }
